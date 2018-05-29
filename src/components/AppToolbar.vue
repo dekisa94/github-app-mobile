1e2629fac81381b7a3ff185bbe4e5fc725e9efd0
dekisa94 <dsimic94@gmail.com>
2018-05-29 21:53:19 +0200

    Task3

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -1,10 +1,13 @@
 <template>
     <v-ons-toolbar>
         <div class="left">
-            <slot name="left"></slot>
+            <slot name="left">Levi slot</slot>
+        </div>
+        <div class="center">
+            {{title}}
         </div>
         <div class="right">
-            <slot name="right"></slot>
+            <slot name="right">Desni slot</slot>
         </div>
     </v-ons-toolbar>
 </template>
