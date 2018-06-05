3393fae8b43220f66507b510f933568ea2fdfa4b
DejanS <dsimic94@gmail.com>
2018-06-05 15:44:52 +0200

    Token delete

--- src/service/GitService.js
+++ src/service/GitService.js
@@ -3,8 +3,8 @@ import Axios from "axios";
 export default class GitService {
   constructor() {
     Axios.defaults.baseURL = "https://api.github.com/";
-    Axios.defaults.headers.common["Authorization"] =
-      "Bearer " + "bba1605e7ca3818a7dcf2b4d0d3afd667a2a65fd";
+    // Axios.defaults.headers.common["Authorization"] =
+    //   "Bearer " + "bba1605e7ca3818a7dcf2b4d0d3afd667a2a65fd";
   }
   gitRepos(username) {
     return Axios.get(`users/${username}/repos`, username);
